edf80833f79d03edbcc6259b52f259d1deab07b5
filinto <filinto.urh@gmail.com>
2019-07-26 16:37:52 +0100

    make it better

--- app/Http/Controllers/ProcurarController.php
+++ app/Http/Controllers/ProcurarController.php
@@ -9,11 +9,12 @@ use App\Compra;
 class ProcurarController extends Controller
 {
 
-    public function search(){
+    public function search($search){
+   
+        $search = urldecode($search);
+        $search1= $search;
 
-        $search = urldecode("123");
-
-        $ticket = compra::where('nif',$search)->first();
+        $ticket = compra::where('nif',$search1)->first();
         
 
         return view('modal', compact('ticket'));
