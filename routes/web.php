1b70e2588f92c596ab768a0adfc1897363c66bec
filinto <filinto.urh@gmail.com>
2019-07-26 17:02:31 +0100

    make it better

--- routes/web.php
+++ routes/web.php
@@ -98,9 +98,9 @@ Route::get("procurar/search/{search}", "ProcurarController@search");
 Route::get('procurar/searchredirect', function(){
      
     /* Nuevo: si el argumento search está vacío regresar a la página anterior */
-    //if (empty(Input::get('search'))) return redirect()->back();
+    if (empty(Input::get('search'))) return redirect()->back();
     
-    $search = urlencode(e(Input::get('search')));
+    $search = urlencode((Input::get('search')));
     $route = "procurar/search/$search";
     return redirect($route);
 });
