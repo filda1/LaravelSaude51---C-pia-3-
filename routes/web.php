2d63c281f163571c99221bae8b643a1b0a58b078
filinto <filinto.urh@gmail.com>
2019-07-26 17:05:41 +0100

    make it better

--- routes/web.php
+++ routes/web.php
@@ -100,7 +100,7 @@ Route::get('procurar/searchredirect', function(){
     /* Nuevo: si el argumento search está vacío regresar a la página anterior */
     if (empty(Input::get('search'))) return redirect()->back();
     
-    $search = urlencode((Input::get('search')));
+    $search = Input::get('search');
     $route = "procurar/search/$search";
     return redirect($route);
 });
