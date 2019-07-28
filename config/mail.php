be455699d1ef6adb3e1dd3a90864a4ae666755ac
filda1 <38835920+filda1@users.noreply.github.com>
2019-07-28 19:46:29 +0100

    ...

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'candidaturas.urh@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'filintomeireles@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
