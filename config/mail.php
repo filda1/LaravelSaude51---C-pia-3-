a089400a35af9e395dd425be2df26811ff8ff8a0
filda1 <38835920+filda1@users.noreply.github.com>
2019-07-28 20:00:55 +0100

    ...

--- config/mail.php
+++ config/mail.php
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('filintomeireles@gmail.com'),
+    'username' => env('MAIL_USERNAME'),
 
-    'password' => env('Portugal1979'),
+    'password' => env('MAIL_PASSWORD'),
 
     /*
     |--------------------------------------------------------------------------
